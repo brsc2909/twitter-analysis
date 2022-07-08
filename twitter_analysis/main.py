fd88a99dd65c1bf2baba306b8114d4b3e7b2b0ff
Brendan Scullion <brsc2909@gmail.com>
2022-07-08 01:19:56 +0100

    preliminary results

--- twitter_analysis/main.py
+++ twitter_analysis/main.py
@@ -1,34 +1,132 @@
 import argparse
+from datetime import datetime
+from typing import Tuple
+
+import yaml
+
+from twitter_analysis.database import Database
+from twitter_analysis.sentiment import Sentiment
+from twitter_analysis.twitter import TwitterApiV2, parse_tweet, parse_user
 
 
-def parse_args():
+def parse_date(d: str):
+    return datetime.strptime(d, "%Y-%m-%d")
+
+
+def parse_args() -> Tuple:
     parser = argparse.ArgumentParser(
         description="Analyse Posts from a twitter search",
         formatter_class=argparse.RawTextHelpFormatter,
     )
     parser.add_argument(
-        "--search",
-        dest="search",
-        default="",
-        help="twitter search criteria",
-        required=True,
+        "-q",
+        "--query",
+        dest="query",
+        default=None,
+        help="twitter search query",
     )
     parser.add_argument(
+        "-l",
+        "--lang",
+        dest="lang",
+        default="en",
+        help="Language (default: en)",
+    )
+    parser.add_argument(
+        "-s",
+        "--start",
+        dest="start",
+        default=None,
+        type=parse_date,
+        help="start date (format: yyyy-mm-dd)",
+    )
+    parser.add_argument(
+        "-e",
+        "--end",
+        dest="end",
+        default=None,
+        type=parse_date,
+        help="end date (format: yyyy-mm-dd)",
+    )
+    parser.add_argument(
+        "-n",
+        "--limit",
+        dest="limit",
+        default=None,
+        type=int,
+        help="limit number of top level tweets",
+    )
+    parser.add_argument(
+        "-c",
         "--config",
         dest="config",
-        default="config.yaml",
+        default=".config.yaml",
         help="Path to config file (Default: config.yaml)",
     )
 
     args = parser.parse_args()
 
-    return args.search, args.config
+    return (args.query, args.lang, args.config, args.start, args.end, args.limit)
 
 
 def main():
-    search, config_file = parse_args()
+    query, lang, config_file, start, end, limit = parse_args()
 
-    print(search, config_file)
+    with open(config_file, encoding="utf8") as stream:
+        config = yaml.safe_load(stream)
+        twitter = TwitterApiV2(token=config.get("bearer_token"))
+        models = config["sentiment_models"]
+        sentiment = Sentiment(models.get(lang))
+
+        with Database(f"data_{lang}.db") as db:
+            db.setup()
+
+            def process_batch(tweets: list, users: list, places: dict) -> Tuple:
+
+                parsed_tweets = [parse_tweet(tweet, places) for tweet in tweets]
+
+                sentiments = sentiment.pipeline(
+                    [tweet["tweet_text"] for tweet in parsed_tweets]
+                )
+
+                return (
+                    [
+                        {
+                            **tweet,
+                            "sentiment": sentiment["label"].upper(),
+                            "sentiment_score": sentiment["score"],
+                        }
+                        for tweet, sentiment in zip(parsed_tweets, sentiments)
+                    ],
+                    [parse_user(user) for user in users],
+                )
+
+            # First we get all the top level tweets
+            print(f"Fetching tweets for search: {query}")
+            results = twitter.search(
+                f"{query} -is:retweet lang:{lang}", start=start, end=end, limit=limit
+            )
+            for tweets, users, places in results:
+                print(".", end="")
+                processed_tweets, processed_users = process_batch(tweets, users, places)
+                db.insert_tweets(processed_tweets)
+                db.insert_users(processed_users)
+            print()
+
+            # get replies to top 5% of replied tweets
+            print("fetching top replied tweets.")
+            for conversation in db.get_top_replied(top_percent=5):
+                print(f"fetching replies for conversation: {conversation[2]}")
+                replies = twitter.search_replies(conversation[2])
+
+                for tweets, users, places in replies:
+                    print(".", end="")
+                    processed_replies, processed_users = process_batch(
+                        tweets, users, places
+                    )
+                    db.insert_replies(processed_replies)
+                    db.insert_users(processed_users)
+                print()
 
 
 if __name__ == "__main__":
--- twitter_analysis/twitter.py
+++ twitter_analysis/twitter.py
@@ -1,22 +1,86 @@
-from typing import Iterator
+from datetime import datetime, timedelta
+from enum import Enum
+from time import sleep, time
+from typing import Iterator, Tuple
 
 import requests
-from ratelimit import limits, sleep_and_retry
+from pyrate_limiter import BucketFullException, Duration, Limiter, RequestRate
+
+
+class SearchMethod(Enum):
+    ALL = "all"
+    RECENT = "recent"
+
+
+rate_limits = (
+    RequestRate(1, Duration.SECOND),
+    RequestRate(300, Duration.HOUR / 4),
+)
+
+limiter = Limiter(*rate_limits)
 
 
 class TwitterApiV2:
     """Twitter API V2"""
 
-    _api_rate_limit_period: int = 1000
-    _api_rate_limit: int = 300
+    FIFTEEN_MINUTES: int = 900
+    MAX_REQUESTS: int = 300
 
-    base_url: str = "https://api.twitter.com/2/"
+    base_url: str = "https://api.twitter.com/2"
+
+    user_fields: list[str] = [
+        "created_at",
+        "description",
+        "entities",
+        "id",
+        "location",
+        "name",
+        "pinned_tweet_id",
+        "profile_image_url",
+        "protected",
+        "public_metrics",
+        "url",
+        "username",
+        "verified",
+        "withheld",
+    ]
+    place_fields: list[str] = [
+        "contained_within",
+        "country",
+        "country_code",
+        "full_name",
+        "geo",
+        "id",
+        "name",
+        "place_type",
+    ]
+
+    tweet_fields: list[str] = [
+        "attachments",
+        "author_id",
+        "context_annotations",
+        "conversation_id",
+        "created_at",
+        "entities",
+        "geo",
+        "id",
+        "in_reply_to_user_id",
+        "lang",
+        "public_metrics",
+        "possibly_sensitive",
+        "referenced_tweets",
+        "reply_settings",
+        "source",
+        "text",
+        "withheld",
+    ]
+
+    tweet_expansions: list[str] = ["author_id", "geo.place_id"]
 
     def __init__(self, token) -> None:
         self.headers = self.headers = {"Authorization": f"Bearer {token}"}
 
-    @sleep_and_retry
-    @limits(calls=_api_rate_limit, period=_api_rate_limit_period)
+    @limiter.ratelimit("identity")
     def _make_api_call(self, url: str, url_params: dict) -> dict:
         """Request data from api endpoint with headers
         :param url: string of the url endpoint to make request from
@@ -25,34 +89,155 @@ class TwitterApiV2:
         response = requests.get(url, url_params, headers=self.headers)
 
         output = response.json()
-
-        return output
+        if response.status_code == 200:
+            return output
+        elif response.status_code == 429:
+            raise BucketFullException(
+                "identity", RequestRate(300, Duration.HOUR / 4), Duration.HOUR / 4
+            )
+        else:
+            raise Exception(output)
 
     def get_account_info(self, user_id: str):
         pass
 
-    def search(self, query: str, limit: int) -> Iterator[dict]:
+    def search(
+        self,
+        query: str,
+        start: datetime = datetime.now() - timedelta(days=7),
+        end: datetime = datetime.now() - timedelta(days=1),
+        limit: int = None,
+        method: SearchMethod = SearchMethod.ALL,
+    ) -> Tuple[Iterator[dict], Iterator[dict]]:
         """Search twitter feed
-        :param query:
+        :param query: twitter search query
         """
 
-        url = f"{self.base_url}/tweet/search/all"
+        url = f"{self.base_url}/tweets/search/{method.value}"
         payload = {
             "query": query,
-            "tweet.fields": [],
-            "user.fields": [],
-            "media.fields": [],
-            "expansions": "",
+            "tweet.fields": ",".join(self.tweet_fields),
+            "user.fields": ",".join(self.user_fields),
+            "place.fields": ",".join(self.place_fields),
+            "max_results": 100 if not limit or limit > 100 else limit,
+            "expansions": ",".join(self.tweet_expansions),
         }
+        if start:
+            payload["start_time"] = (start.isoformat() + "Z",)
+        if end:
+            payload["end_time"] = (end.isoformat() + "Z",)
 
-        results = self._make_api_call(url, payload)
-
-        yield results
-
-        while results["meta"].get("next_token", False):
-
-            payload["next_token"] = results["meta"].get("next_token", False)
-
+        try:
+            results = self._make_api_call(url, payload)
+        except BucketFullException as e:
+            print(f"sleeping for {e.meta_info['remaining_time']} seconds")
+            sleep(e.meta_info["remaining_time"] + 1)
             results = self._make_api_call(url, payload)
 
-            yield results
+        tweet_count = 0
+        tweets = results["data"]
+        users = results["includes"]["users"]
+        places = {
+            place["id"]: place["full_name"]
+            for place in results["includes"].get("places", [])
+        }
+        tweet_count += len(tweets)
+
+        yield tweets, users, places
+
+        while results["meta"].get("next_token", False):
+            if limit and limit < tweet_count:
+                break
+
+            payload["next_token"] = results["meta"].get("next_token", False)
+            t = time()
+            try:
+                results = self._make_api_call(url, payload)
+            except BucketFullException as e:
+                print(f"sleeping for {e.meta_info['remaining_time']} seconds")
+                sleep(e.meta_info["remaining_time"] + 1)
+
+            tweets = results["data"]
+            users = results["includes"]["users"]
+            places = {
+                place["id"]: place["full_name"]
+                for place in results["includes"].get("places", [])
+            }
+            tweet_count += len(tweets)
+            yield tweets, users, places
+            # if request takes less than 1 second wait for remainder
+            if time() - t < 1:
+                sleep(1 - (time() - t))
+
+    def search_replies(
+        self, converation_id: int
+    ) -> Tuple[Iterator[dict], Iterator[dict]]:
+        return self.search(
+            query=f"conversation_id:{converation_id} is:reply", start=None, end=None
+        )
+
+
+def parse_tweet(tweet: dict, places: dict) -> Tuple:
+    """id,author,url,tweet_text,timestamp,hashtags,media,urls,location,likes,replies,retweets,quotes"""
+    hashtags = []
+    mentions = []
+    urls = []
+    location = None
+
+    public_metrics = tweet["public_metrics"]
+
+    if "entities" in tweet:
+        hashtags = [hashtag["tag"] for hashtag in tweet["entities"].get("hashtags", [])]
+        mentions = [
+            mention["username"] for mention in tweet["entities"].get("mentions", [])
+        ]
+        urls = [url["expanded_url"] for url in tweet["entities"].get("urls", [])]
+
+    if "geo" in tweet:
+        location = places.get(tweet["geo"]["place_id"])
+    media = []
+    for item in tweet.get("media", []):
+        media_link = item.get("url", None) or item.get("preview_image_url", None)
+        media.append(media_link)
+
+    referenced_tweet = (
+        [
+            i["id"]
+            for i in tweet.get("referenced_tweets", [{"type": None}])
+            if i["type"] == "replied_to"
+        ]
+        or [None]
+    )[0]
+    return {
+        "id": tweet["id"],
+        "parent_id": referenced_tweet,
+        "conversation_id": tweet.get("conversation_id", None),
+        "author": tweet["author_id"],
+        "url": f"https://twitter.com/{tweet['author_id']}/status/{tweet['id']}",
+        "tweet_text": tweet["text"],
+        "timestamp": tweet["created_at"],
+        "hashtags": ",".join(hashtags) or None,
+        "mentions": ",".join(mentions) or None,
+        "media": ",".join(media) or None,
+        "urls": ",".join(urls),
+        "location": location,
+        "likes": public_metrics.get("like_count", 0),
+        "replies": public_metrics.get("reply_count", 0),
+        "retweets": public_metrics.get("retweet_count", 0),
+        "quotes": public_metrics.get("quote_count", 0),
+    }
+
+
+def parse_user(user: dict):
+    """id, username, verified, location, following, followers, date_joined"""
+    public_metrics = user["public_metrics"]
+    return {
+        "id": user["id"],
+        "username": user["username"],
+        "verified": user["verified"],
+        "location": user.get("location"),
+        "following": public_metrics["following_count"],
+        "followers": public_metrics["followers_count"],
+        "date_joined": user["created_at"],
+        "bio": user["description"],
+    }
